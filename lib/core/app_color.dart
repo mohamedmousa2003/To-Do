e9e3215c2dcaedc5606327a7b37b88d61a603a28
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-04-29 10:43:32 +0300

    app Color

---
+++ lib/core/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// light color
+Color primaryLight = const Color(0xffDFECDB);
+Color primaryDark = const Color(0xff060E1E);
+Color grayColor = const Color(0xff61E757);
+Color blueColor = const Color(0xff5D9CEC);
+Color whiteColor = const Color(0xffFFFFFF);
+Color redColor = const Color(0xffEC4B4B);
