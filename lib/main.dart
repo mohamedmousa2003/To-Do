0b2c166844a6b1c84e8355419097321c311314d2
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-04-29 11:26:36 +0300

    Internationalizing app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:todo/pages/splash/splash_screen.dart';
 
 void main() {
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TO DO',
       initialRoute: SplashScreen.routeName,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale("en"),
       routes: {SplashScreen.routeName: (context) => const SplashScreen()},
     );
   }
