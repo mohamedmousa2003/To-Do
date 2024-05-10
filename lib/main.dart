8d7512433b7960038e525f3186cd01742b9ad9fa
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-05-10 18:14:12 +0300

    add collection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
@@ -6,7 +7,13 @@ import 'package:todo/core/theme.dart';
 import 'package:todo/pages/bottom_navigationBar/home_layout.dart';
 import 'package:todo/pages/splash/splash_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
       create: (context) => MyProvider(), child: const MyApp()));
 }
---
+++ lib/models/task_model.dart
@@ -0,0 +1,38 @@
+class TaskModel {
+  String? id;
+
+  String? title;
+  String? description;
+  DateTime? date;
+  bool? isDone;
+
+  TaskModel({
+    this.id = "",
+    required this.title,
+    required this.description,
+    required this.date,
+    this.isDone = false,
+  });
+
+  // TO CONVERT object to Map
+  Map<String, dynamic> toJason() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'date': date?.millisecondsSinceEpoch,
+      'isDone': isDone,
+    };
+  }
+
+  // TO CONVERT Map to object
+
+  TaskModel.fromJson(Map<String, dynamic> data)
+      : this(
+          id: data['id'],
+          title: data['title'],
+          description: data['description'],
+          date: DateTime.fromMillisecondsSinceEpoch(data['date']),
+          isDone: data['isDone'],
+        );
+}
