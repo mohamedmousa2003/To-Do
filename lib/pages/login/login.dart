0ad352ac0b0ef7900dd6e55c5a776cf52ffbd9e6
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-05-13 19:30:25 +0300

    add login branch

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -5,6 +5,10 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
