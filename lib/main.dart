22397a7d19631b99f5ead31116a028b31e9eda82
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-05-18 21:29:36 +0300

    r

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ import 'package:todo/pages/splash/splash_screen.dart';
 
 import 'firebase_options.dart';
 
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
