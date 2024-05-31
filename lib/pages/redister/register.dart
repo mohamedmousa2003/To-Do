ef705859c54fef39f3f8fe9b101e0f73d3fbdcb7
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-05-31 23:37:22 +0300

    register

--- lib/pages/redister/register.dart
+++ lib/pages/redister/register.dart
@@ -1,10 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:todo/core/app_color.dart';
+
+import '../../widget/tap_text.dart';
 
 class Register extends StatelessWidget {
-  const Register({super.key});
+  Register({super.key});
+
+  static String routeName = "register";
+  var full_Name_Controller = TextEditingController();
+  var password_Controller = TextEditingController();
+  var confirmation_Password_Controller = TextEditingController();
+  var email_Controller = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var theme = Theme.of(context);
+    var mediaQuery = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/background_login.png",
+            fit: BoxFit.fill,
+            width: double.infinity,
+          ),
+          Positioned(
+              top: mediaQuery.height * 0.1,
+              left: mediaQuery.width * 0.42,
+              child: Text(
+                "Login",
+                style: theme.textTheme.bodyLarge,
+                textAlign: TextAlign.center,
+              )),
+          Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.35,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomTextField(
+                          controller: full_Name_Controller,
+                          hint: "Full Name",
+                          onValidate: (text) {
+                            if (text == null || text.trim().isEmpty) {
+                              return "please enter your full name";
+                            }
+                            return null;
+                          },
+                        ),
+                        CustomTextField(
+                          controller: email_Controller,
+                          hint: "Email",
+                          onValidate: (text) {
+                            if (text == null || text.trim().isEmpty) {
+                              return "please enter your email";
+                            }
+                            final bool emailValid = RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[com]+")
+                                .hasMatch(text);
+                            if (!emailValid) {
+                              return "Please enter valid email content @[a-zA-Z0-9]";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType
+                              .emailAddress, // as to show @ in keyboard
+                        ),
+                        CustomTextField(
+                          controller: password_Controller,
+                          hint: "Password",
+                          isPassword: true,
+                          onValidate: (text) {
+                            if (text == null || text.trim().isEmpty) {
+                              return "please enter your password";
+                            }
+                            if (text.length < 6) {
+                              return "please enter password not less 6 char";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType
+                              .emailAddress, // as to show @ in keyboard
+                        ),
+                        CustomTextField(
+                          controller: confirmation_Password_Controller,
+                          isPassword: true,
+                          hint: "Confirm Password",
+                          onValidate: (text) {
+                            if (text == null || text.trim().isEmpty) {
+                              return "please enter your confirm Password";
+                            }
+                            if (text != password_Controller.text) {
+                              return "please does not match";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType
+                              .emailAddress, // as to show @ in keyboard
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                          ),
+                          onPressed: () {
+                            register();
+                          },
+                          child: Text(
+                            "register",
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("Already have an account"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void register() {
+    if (formKey.currentState?.validate() == true) {
+      // register
+    }
   }
 }
+
+/*
+  */
--- lib/widget/tap_text.dart
+++ lib/widget/tap_text.dart
@@ -1,42 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import '../core/app_color.dart';
-class CustomTextFormField extends StatelessWidget {
-  CustomTextFormField(
-      {super.key,
-      required this.controller,
-      required this.title,
-      required this.validator,
-      this.maxLines});
+class CustomTextField extends StatefulWidget {
+  final TextEditingController? controller;
+  final bool? isPassword;
+  final String? hint;
+  final bool? enabled;
+  final int? maxLines, minLines, maxLength;
+  final String? obscuringCharacter, value;
+  final String? Function(String?)? onValidate;
+  final void Function(String?)? onChanged, onFieldSubmitted, onSaved;
+  final void Function()? onEditingComplete, onTap;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+  final Widget? suffixWidget, prefixIcon;
+  final IconData? icon;
+  final TextInputAction? action;
+  final FocusNode? focusNode;
 
-  final TextEditingController controller;
-  final FormFieldValidator<String> validator;
-  String title;
-  int? maxLines = 1;
+  const CustomTextField({
+    super.key,
+    this.controller,
+    this.isPassword,
+    this.hint,
+    this.enabled,
+    this.obscuringCharacter,
+    this.value,
+    this.onValidate,
+    this.onChanged,
+    this.onFieldSubmitted,
+    this.onEditingComplete,
+    this.onSaved,
+    this.onTap,
+    this.maxLines = 1,
+    this.minLines = 1,
+    this.maxLength,
+    this.keyboardType,
+    this.inputFormatters,
+    this.suffixWidget,
+    this.icon,
+    this.prefixIcon,
+    this.action,
+    this.focusNode,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Scaffold(
-      body: TextFormField(
-        controller: controller,
-        style: theme.textTheme.bodyMedium,
-        validator: validator,
-        cursorColor: Colors.black,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        controller: widget.controller,
+        initialValue: widget.value,
+        validator: widget.onValidate,
+        onChanged: widget.onChanged,
+        onEditingComplete: widget.onEditingComplete,
+        onFieldSubmitted: widget.onFieldSubmitted,
+        onSaved: widget.onSaved,
+        onTap: widget.onTap,
+        maxLines: widget.maxLines,
+        minLines: widget.minLines,
+        maxLength: widget.maxLength,
+        obscureText: widget.isPassword ?? false ? obscureText : false,
+        obscuringCharacter: '*',
+        keyboardType: widget.keyboardType,
+        inputFormatters: widget.inputFormatters,
+        enabled: widget.enabled,
+        style: theme.textTheme.bodyLarge!.copyWith(
+          color: theme.primaryColor,
+        ),
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        textInputAction: widget.action ?? TextInputAction.done,
+        focusNode: widget.focusNode,
         decoration: InputDecoration(
-          //hintText: local.enter_your_task,
-          label: Text(title),
-          labelStyle: theme.textTheme.bodySmall,
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(
-              color: blueColor,
+          fillColor: Colors.white,
+          filled: true,
+          suffixIcon: widget.isPassword ?? false
+              ? InkWell(
+                  onTap: () {
+                    setState(() {
+                      obscureText = !obscureText;
+                    });
+                  },
+                  child: Icon(
+                    obscureText ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.grey,
+                  ),
+                )
+              : widget.suffixWidget,
+          prefixIcon: widget.prefixIcon,
+          hintText: widget.hint,
+          hintStyle: TextStyle(fontSize: 18, color: Colors.grey.shade600),
+          counterText: "",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              color: Colors.white,
+              width: 1,
             ),
           ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
+          // suffix: isPass widget.suffixWidget,
+          contentPadding: const EdgeInsets.only(
+            top: 15,
+            left: 10,
+            right: 10,
+            bottom: 15,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
             borderSide: BorderSide(
-              color: blueColor,
+              color: theme.primaryColor,
+              width: 1,
+            ),
+          ),
+          errorStyle: const TextStyle(color: Colors.red),
+          errorMaxLines: 4,
+          errorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              color: Colors.red,
+              width: 1,
             ),
           ),
         ),
